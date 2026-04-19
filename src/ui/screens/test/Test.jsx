92f0c02a52dccc2cc087b6c657e3fed0f0dd090d
eleadn <eleano.dn@gmail.com>
2026-04-19 21:13:28 +0200

    Refactor Test component to display numberTest value alongside its name

--- src/ui/screens/test/Test.jsx
+++ src/ui/screens/test/Test.jsx
@@ -1,10 +1,13 @@
+import useGameStore from "../../../store/gameStore";
 import useLocale from "../../hooks/useLocale";
 
 function Test() {
 	const tr = useLocale();
+	const numberTest = useGameStore((s) => s.test.resources.numberTest);
+
 	return (
 		<>
-			<p>{tr("numberTest.name")}</p>
+			<p>{tr("numberTest.name") + " : " + `${numberTest}`}</p>
 		</>
 	);
 }
