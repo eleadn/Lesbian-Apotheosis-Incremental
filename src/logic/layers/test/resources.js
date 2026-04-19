660b511554f52beb076321e71b2f5e0fb2d1d8bb
eleadn <eleano.dn@gmail.com>
2026-04-19 17:56:02 +0200

    Add baseValue to NUMBER_TEST resource in TEST_RESOURCES

--- src/logic/layers/test/resources.js
+++ src/logic/layers/test/resources.js
@@ -1,7 +1,9 @@
 import TEST_IDS from "./ids";
 
 const TEST_RESOURCES = {
-	[TEST_IDS.resources.NUMBER_TEST]: {},
+	[TEST_IDS.resources.NUMBER_TEST]: {
+		baseValue: 0,
+	},
 };
 
 export default TEST_RESOURCES;
